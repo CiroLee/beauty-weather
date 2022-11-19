13a0d878f33be6bcca61b9ecdac640cc00fb6b4d
CiroLee <ciromail@126.com>
2022-11-19 15:40:15 +0800

    feat: modify router cinfig

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,21 +3,26 @@ import Layout from '@/layout';
 import Home from '@/pages/home';
 import List from '@/pages/list';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          id: 'home',
+          element: <Home />,
+        },
+        {
+          path: '/list',
+          id: 'list',
+          element: <List />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        id: 'home',
-        element: <Home />,
-      },
-      {
-        path: '/list',
-        id: 'list',
-        element: <List />,
-      },
-    ],
+    basename: '/beauty-weather',
   },
-]);
+);
