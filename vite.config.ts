47ab788118c0ad7cf9f5475b0d4faafc6edaf47e
CiroLee <ciromail@126.com>
2022-12-13 12:27:42 +0800

    feat: update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import px2vw from '@yuo/postcss-px2vw';
 import autoprefixer from 'autoprefixer';
